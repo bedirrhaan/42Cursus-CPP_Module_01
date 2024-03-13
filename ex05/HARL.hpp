858beffddc2afbdad2a4051ca9f58247b6b69036
bedirrhaan <bedirrhaan@gmail.com>
2024-03-13 14:30:41 +0300

    42

---
+++ ex05/HARL.hpp
@@ -0,0 +1,19 @@
+#ifndef HARL_HPP
+# define HARL_HPP
+# include <iostream>
+
+class HARL
+{
+    private:
+        void    debug(void);
+        void    info(void);
+        void    warning(void);
+        void    error(void);
+
+    public:
+        HARL();
+        ~HARL();
+        void    complain(std::string level);
+};
+
+#endif
---
+++ ex05/main.cpp
@@ -0,0 +1,22 @@
+#include "HARL.hpp"
+
+int main(int ac, char **av)
+{
+    HARL test;
+
+    switch (ac)
+    {
+        case 1:
+            test.complain("DEBUG");
+            test.complain("INFO");
+            test.complain("WARNING");
+            test.complain("ERROR");
+            break;
+        case 2:
+            test.complain(av[1]);
+            break;
+        default:
+            break;
+    }
+    return (0);
+}
